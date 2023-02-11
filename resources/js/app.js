a3e585d6291a78efbcd065c28a409db240c56b42
joy2362 <abdullahzahidjoy@gmail.com>
2023-02-12 03:05:09 +0600

    add pinia

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,8 @@
 import './bootstrap';
-
 import { createApp } from 'vue';
 import { createRouter, createWebHistory } from 'vue-router';
 import {routes} from "./routes/router";
-
+import { createPinia } from 'pinia'
 import App from './App.vue';
 
 const router = createRouter({
@@ -11,7 +10,9 @@ const router = createRouter({
     routes
 })
 
+const pinia = createPinia()
 
 const app = createApp(App)
 app.use(router);
+app.use(pinia);
 app.mount("#app");
